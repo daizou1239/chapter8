6b7f9c9a101a4b7cd216d55b7eef1768d0c8855c
OshimaDaizou <241111@std.hi-joho.ac.jp>
2025-06-09 12:31:59 +0900

    chapter8

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,22 @@
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        Hero h = new Hero();
-        h.name = "ミナト";
-        h.hp = 100;
+        //Sword s = new Sword();
+       // s.name = "炎の剣";
+       // s.damage = 10;
+
+        Hero h1 = new Hero("ミナト");
+        //h1.name = "ミナト";
+        System.out.println(h1.hp);
+        System.out.println(h1.name);
+        /*Hero h2 = new Hero();
+        h2.name = "アカサ";
+        h2.hp = 100;
+        Wizard w = new Wizard();
+        w.name = "スガワラ";
+        w.hp = 50;
+
+        /*System.out.println("現在の武器は" + h.sword.name);
 
         Matango m1 = new Matango();
         m1.hp = 50;
@@ -21,6 +34,8 @@ public class Main {
         h.sit(25);
         h.run();
 
-
+        w.heal(h1);
+        w.heal(h2);
+        w.heal(h2);*/
     }
 }
